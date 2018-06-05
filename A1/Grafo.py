997f0741cea0a68819b87bd87bc284f25eab78ca
hugo issao uraga <hugouraga61@gmail.com>
2018-06-05 11:33:07 -0300

    A1

--- A1/Grafo.py
+++ A1/Grafo.py
@@ -1,16 +1,40 @@
 from Lista import Lista
 
-class ListaAdjacentes:
-    def __init__(self,quantVertices = 0):
+class Grafo:
+    def __init__(self,quantVertices = 0,grafoDirecionado=False,pesoAresta=None):
         self.quantVertices = quantVertices
         self.listaVertices = Lista()
-
+        self.grafoDirecionado = grafoDirecionado
+        self.pesoAresta = pesoAresta
         for elemento in range(quantVertices):
             self.listaVertices.inserir([])
+
     def vazia(self):
         return self.quantVertices == 0
-    def inserirAresta(self,verticeA,verticeB,grafoDirecionado=True):
-        self.listaVertices.inserirAres(verticeA,verticeB)
+
+    def inseAres(self,verticeA,verticeB,grafoDirecionado,peso=1):
+        if self.grafoDirecionado == False:
+            self.listaVertices.inserirAresNaoDire(verticeA,verticeB)
+        elif self.grafoDirecionado == True:
+            self.listaVertices.inserirAresDire(verticeA,verticeB)
+
+    def verifica(self,verticeA,verticeB):
+        aux = self.listaVertices.verificaAresta(verticeA,verticeB)
+        return aux
+
+    def removeAresta(self,verticeA,verticeB):
+        self.listaVertices.removeAresta(verticeA,verticeB)
+
+    def grauEntrada(self,vertice):
+        pass
+
+    def grauSaida(self,vertice):
+        aux = self.listaVertices.grauSaida(vertice)
+        return aux
+
+    def listaAdjacentes(self,vertice):
+        lista = self.listaVertices.listaAd(vertice)
+        return lista
 
     def __str__(self):
         #return self.listaVertices.__str__()
--- A1/Lista.py
+++ A1/Lista.py
@@ -7,47 +7,110 @@ class Lista:
     def __init__(self):
         self.primeiro = self.ultimo = No()
         self.tamanhoLista = 0
+
     def vazia(self):
         return self.primeiro == self.ultimo
+
     def inserir(self,item):
         self.ultimo.proximo = No(item)
         self.ultimo = self.ultimo.proximo
         self.tamanhoLista += 1
-    def inserirInicio(self, item):
-        self.primeiro.proximo = No(item,self.primeiro.proximo)
-        if self.vazia():
-            self.ultimo = self.primeiro.proximo
-        self.tamanhoLista += 1 
-    def inserirOrdenado(self,item):
-        if vazia():
-            self.inserir(item)
-        anteiror = self.primeiro
-        atual = self.primeiro.proximo
-        while not atual is None and atual.item < item:
-            anterior = atual
-            atual = anteiror.proximo
-        anterior.proximo = No(item,atual)
-        if atual is None:
-            self.ultimo = anteiror.proximo
-        self.tamanhoLista += 1
-    def inserirAres(self,verticeA,verticeB):
+
+    def inserirAresNaoDire(self,verticeA,verticeB,peso=1):
         aux = self.primeiro.proximo
         verificador = 0
         while verificador < verticeA and not aux is None:
             aux = aux.proximo
             verificador += 1
-        aux.item.append(verticeB)
+        aux.item.append((verticeB,peso))
         verificador = 0
         aux = self.primeiro.proximo
         while verificador < verticeB and not aux is None:
             aux = aux.proximo
             verificador += 1
-        aux.item.append(verticeA)
+        aux.item.append((verticeA,peso))
+
+    def insereAresDire(self,verticeA,verticeB,peso=1):
+        aux = self.primeiro.proximo
+        verificador = 0
+        while verificador < verticeA and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        aux.item.append((verticeB,peso))
+
+    def removeAresta(self,verticeA,verticeB):
+        aux = self.primeiro.proximo
+        verificador = 0
+        while verificador < verticeA and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        listaVerticeA = aux.item
+
+        for elementos in listaVerticeA:
+            if elementos[0] == verticeB:
+                item = elementos
+                listaVerticeA.remove(item)
+
+        aux = self.primeiro.proximo
+        verificador = 0
+        while verificador < verticeB and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        listaVerticeB = aux.item
+        print(listaVerticeB)
+        for elementos in listaVerticeB:
+            if elementos[0] == verticeA:
+                item = elementos
+                listaVerticeB.remove(item)
+    def verificaAresta(self,verticeA,verticeB):
+        aux = self.primeiro.proximo
+        verificador = 0
+        while verificador < verticeA and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        listaVerticeA = aux.item
+        verificador = 0
+        aux = self.primeiro.proximo
+        while verificador < verticeB and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        listaVerticeB = aux.item
+        existe = 0
+        for primeiro in listaVerticeA:
+            if primeiro[0] == verticeB:
+                existe += 1
+        for segundo in listaVerticeB:
+            if segundo[0] == verticeA:
+                existe += 1
+        if existe != 0:
+            return True
+        else:
+            return False
+
+    def grauSaida(self,vertice):
+        aux = self.primeiro.proximo
+        verificador = 0
+        while verificador < vertice and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        listaTam = len(aux.item)
+        return listaTam
+
+    def listaAd(self,vertice):
+        aux = self.primeiro.proximo
+        verificador  = 0
+        while verificador < vertice and not aux is None:
+            aux = aux.proximo
+            verificador += 1
+        lista = aux.item
+        return lista
+
     def pesquisa(self, item):
         aux = self.primeiro.proximo
         while not aux is None and aux.item != item:
             aux = aux.proximo
         return aux is None and None or aux.item
+
     def removerInicio(self):
         if self.vazia():
             return None
@@ -59,6 +122,7 @@ class Lista:
         aux.proximo = None
         del aux
         return item
+
     def removerFim(self):
         if self.vazia():
             return None
@@ -71,6 +135,7 @@ class Lista:
         ultimo.proximo = None
         del aux
         return item
+
     def __str__(self):
         s = "["
         aux = self.primeiro.proximo
@@ -80,6 +145,7 @@ class Lista:
         s = s.strip(",")
         s += "]"
         return s
+
     def __getitem__(self, index):
         if self.vazia():
             return IndexError("A lista se encontra vazia")
@@ -92,5 +158,6 @@ class Lista:
             ponteiro += 1
         if ponteiro == index:
             return aux.valor
+
     def __len__(self):
         return self.tamanhoLista
